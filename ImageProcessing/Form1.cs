a7ebf1ff4419b38b0085564e7b964f13f67c5067
Александра Никитина <aglerika@outlook.com>
2020-07-07 16:35:15 +0300

    New method Get Pixels

--- ImageProcessing/Form1.cs
+++ ImageProcessing/Form1.cs
@@ -24,10 +24,44 @@ namespace ImageProcessing
         {
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-
-                pictureBox1.Image = new Bitmap(openFileDialog1.FileName);
+                pictureBox1.Image = null;
+                _bitmaps.Clear();
+                Bitmap bitmap = new Bitmap(openFileDialog1.FileName);
+                RunProcessing(bitmap);
             }
         }
+
+        private void RunProcessing(Bitmap bitmap)
+        {
+
+        }
+
+        private List<Pixel> GetPixels(Bitmap bitmap)
+        {
+            var pixels = new List<Pixel>(bitmap.Width * bitmap.Height);
+
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                for(int x=0; x < bitmap.Width; x++)
+                {
+                    var pixel = new Pixel()
+                    {
+                        Color = bitmap.GetPixel(x, y),
+                        Point = new Point()
+                        {
+                            X = x,
+                            Y = y
+                        }
+                    };
+
+                    pixels.Add(pixel);
+                }
+            }
+
+            return pixels;
+        }
+
+
 
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
